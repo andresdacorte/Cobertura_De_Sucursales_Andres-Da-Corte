99fdb85acc03e426b31fc4b2095965dbc7f38588
andresdacorte <andres.dacorte@correo.unimet.edu.ve>
2024-10-15 16:21:13 -0400

    Creacion del Git

---
+++ src/cobertura_de_sucursales_andres/da/corte/Cobertura_De_Sucursales_AndresDaCorte.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cobertura_de_sucursales_andres.da.corte;
+
+/**
+ *
+ * @author dacor
+ */
+public class Cobertura_De_Sucursales_AndresDaCorte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
